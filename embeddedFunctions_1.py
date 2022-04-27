34961a943ef98f2675a527a5ad1057a88b174871
Bekir EMANET <44875578+bekiremanet@users.noreply.github.com>
2022-04-27 14:01:24 +0300

    Update embeddedFunctions_1.py

--- embeddedFunctions_1.py
+++ embeddedFunctions_1.py
@@ -1,22 +1,21 @@
 from functools import reduce
 
 measurement = reduce(lambda x,y : x*y, [12,18,2,3]) # REDUCE FUNCTION
+print(measurement)  # output => 1296
 
-print(measurement)
-
-# ---------------------------------------------------- #
-
-enFazla = max([78,54,78,99,145,556])   # MAX. FUNCTION
-
-print(enFazla)
 
 # ---------------------------------------------------- #
 
-filterFunc = list(filter(lambda x : x % 2  == 0 , [0,2,4,12,54,31,17])) # FILTER FUNCTION
+enFazla = max([78,54,78,99,145,556])   # MAX. FUNCTION, 
+print(enFazla) # output => 556
 
-print(filterFunc)
+# ---------------------------------------------------- #
 
-print(type(filterFunc))
+filterFunc = list(filter(lambda x : x % 2  == 0 , [0,2,4,12,54,31,17])) # FILTER FUNCTION, 
+print(filterFunc) # output => [0,2,4,12,54]
+
+
+print(type(filterFunc))  # output => <class 'list'>
 
 # ---------------------------------------------------- #
  
@@ -25,4 +24,4 @@ liste_2 = [1,3,5,7,9,11,13]
 
 zipFunc = list(zip(liste_1, liste_2))  # ZIP FUNCTION
 
-print(zipFunc)
+print(zipFunc)   # output => [(0, 1), (2, 3), (4, 5), (6, 7), (8, 9)]
